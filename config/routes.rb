3def09b1df74520aa4ba0478ab018681ccf7bb72
Michael Cai <michaeldcai@gmail.com>
2014-07-07 17:56:27 -0400

    Added the nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "projects" => "pages#projects"
+  get "photography" => "pages#photography"
+  get "blog" => "pages#blog"
+  get "contact" => "pages#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
